d611b15abffffad7bbf447b136113c733e6aa1ea
Catherine Gloria <catherinegloria@gmail.com>
2021-02-28 12:30:37 -0800

    fadeout

---
+++ app.js
@@ -0,0 +1,10 @@
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
+
+  const startGame = () => {
+    const playBtn = document.querySelector('.intro button')
+    const introScreen = document.querySelector('.intro');
+    const match = document.querySelector('.match');
+  }
+}
